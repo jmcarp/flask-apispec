817f97b8b2e68ac11078eb88970ecaa7b80cc0f7
cliedeman <ciaranliedeman@gmail.com>
2016-02-02 00:05:45 +0200

    Allow enabling a auth key token for swagger-ui

--- flask_apispec/__init__.py
+++ flask_apispec/__init__.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
+
+__version__ = '0.3.2'
+
 from flask_apispec.views import ResourceMeta, MethodResource
 from flask_apispec.annotations import doc, wrap_with, use_kwargs, marshal_with
 from flask_apispec.extension import FlaskApiSpec
 from flask_apispec.utils import Ref
-
-__version__ = '0.3.2'
 
 __all__ = [
     'doc',
