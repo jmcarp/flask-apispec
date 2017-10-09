1382d9673ec573b201731ac93eb74dc52a9d64e6
Steven Loria <sloria1@gmail.com>
2017-10-08 21:08:17 -0400

    Fix releasing with static assets

--- flask_apispec/__init__.py
+++ flask_apispec/__init__.py
@@ -4,7 +4,7 @@ from flask_apispec.annotations import doc, wrap_with, use_kwargs, marshal_with
 from flask_apispec.extension import FlaskApiSpec
 from flask_apispec.utils import Ref
 
-__version__ = '0.4.1'
+__version__ = '0.4.1.post0'
 __all__ = [
     'doc',
     'wrap_with',
