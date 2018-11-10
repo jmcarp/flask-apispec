2fff3d58788f4558f67107c79b22f8566b3c2936
Mischa Spiegelmock <revmischa@cpan.org>
2018-11-10 10:15:49 +0200

    Account for SCRIPT_NAME when generating documentation if application is not hosted on /

--- flask_apispec/extension.py
+++ flask_apispec/extension.py
@@ -84,7 +84,22 @@ class FlaskApiSpec(object):
 
         self.app.register_blueprint(blueprint)
 
+    def fix_base_path(self):
+        """Lazily set OpenAPI basePath for each request.
+
+        This is to reflect the path root the application is running under.
+        c.f. SCRIPT_NAME https://www.python.org/dev/peps/pep-0333/#environ-variables
+        """
+        if not flask.has_request_context():
+            # not in request
+            return
+        if not flask.request.script_root:
+            # not running under different root
+            return
+        self.spec.options['basePath'] = flask.request.script_root
+
     def swagger_json(self):
+        self.fix_base_path()
         return flask.jsonify(self.spec.to_dict())
 
     def swagger_ui(self):
