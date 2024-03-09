d537a094ed0e25efcdcfc9ebbb36500a2b715582
Marat Sharafutdinov <decaz89@gmail.com>
2024-03-09 17:16:07 +0300

    Add packaging dependency

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ setup(
     zip_safe=False,
     keywords='flask marshmallow webargs apispec',
     python_requires=">=3.8",
+    dependencies=["packaging>=17.0"],
     test_suite='tests',
     project_urls={
         'Bug Reports': 'https://github.com/jmcarp/flask-apispec/issues',
